516817220fcacb1a777da783f762731013471bb5
Suyash Singh <0808cs211182.ies@ipsacademy.org>
2025-02-28 12:48:12 +0530

    fixed import

---
+++ client/src/config/firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBiETj3nq8mxFF73iynII-jB-PSdQLlzBA",
+  authDomain: "staysphere-tenant.firebaseapp.com",
+  projectId: "staysphere-tenant",
+  storageBucket: "staysphere-tenant.firebasestorage.app",
+  messagingSenderId: "136933061165",
+  appId: "1:136933061165:web:f94cb8b1deeec534657ea4",
+  measurementId: "G-L32DXBH8H4",
+};
+export default firebaseConfig;
--- client/src/utility/Firebase.js
+++ client/src/utility/Firebase.js
@@ -1,4 +1,5 @@
 import firebaseConfig from "../config/firebaseConfig";
+
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
