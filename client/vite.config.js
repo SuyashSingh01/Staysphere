e822a5d14f2a9730c7f780b50f0c780d2be11d0f
Suyash Singh <0808cs211182.ies@ipsacademy.org>
2025-02-28 13:18:29 +0530

    fixex the build

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,8 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { fileURLToPath } from "url";
 import path from "path";
 
-// https://vite.dev/config/
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 export default defineConfig({
   plugins: [react()],
   resolve: {
@@ -11,13 +14,14 @@ export default defineConfig({
     },
   },
   optimizeDeps: {
-    include: ["swiper"],
+    include: ["swiper", "swiper/react"],
   },
   test: {
     globals: true,
     environment: "jsdom",
+    setupFiles: "./vitest.setup.js",
   },
   build: {
-    outDir: "public", // or "dist" based on your setup
+    outDir: "dist",
   },
 });
