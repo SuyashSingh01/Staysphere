2356ae06d7df8fc0799f03ba20ff2d23a9c40a9e
Suyash Singh <0808cs211182.ies@ipsacademy.org>
2025-03-08 20:28:05 +0530

    fixes url

--- server/src/index.js
+++ server/src/index.js
@@ -28,14 +28,12 @@ const server = http.createServer(app);
 
 // socket constructor function  to create a new instance of soket service
 new SocketService(server);
-// get the io instance from the socket service
-// const io = socketservice.getIO();
 
 // Middleware
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: process.env.CLIENT_URL,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
